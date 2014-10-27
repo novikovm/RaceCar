802caaeed150d76d9706c6445b412b05d840a7bc
Mykyta Novikov <nekitko123@gmail.com>
2014-10-27 09:51:02 +0100

    second

---
+++ cars.php
@@ -0,0 +1,22 @@
+interface RaceCarInterface
+{
+    public function getVMax();
+    public function getAcceleration();
+}
+
+interface EngineInterface
+{
+    public function getHP();
+}
+
+	class RaceCar{
+	public $silnik;
+	public $max_predkosc = 
+		public function getVMax(){
+	
+		}
+	}
+
+	class Engine{
+	public $silnik = 200;
+	}
